2d13c3660c011b98fcb054cdd5679352b4400b71
jayamehra077-ui <jayamehra077@gmail.com>
2026-04-07 15:17:59 +0530

    code

--- p2.py
+++ p2.py
@@ -1,2 +1,16 @@
-//write a program to take input fom user and print all factors of input
+//write a program to take input and check prime or not//
+n=int(input("enter number to check prime or not: "))
+if n<=1:
+    print("invalid input")
+else:
+    for i in range(2,n+1):
+       if n%i==0:
+        print("not a prime number")
+        break
+    else:
+        print("prime")
+    
+    
+
+
 
